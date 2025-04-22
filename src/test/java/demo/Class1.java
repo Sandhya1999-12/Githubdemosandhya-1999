6900aaee6b7ecba97aee5567d57a0a9d4cb6e9cb
Sandhya1999-12 <sandhyapawar477@gmail.com>
2025-04-22 17:29:39 +0530

    first commit

---
+++ src/test/java/demo/Class1.java
@@ -0,0 +1,12 @@
+package demo;
+
+public class Class1 {
+	void method1()
+	{
+		System.out.println("git hub demo");
+	}
+    void method2()
+    {
+    	System.out.println("git hub demo");
+    }
+}
